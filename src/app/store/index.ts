8c4a8932489a84aeab8cf359f3720315de6c9785
Amir Kadenov <amirkadenov@gmail.com>
2025-02-10 09:29:45 +0500

    add: store

---
+++ src/app/store/index.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+type Store = {
+  favorites: string[]
+  addFavorite: (city: string) => void
+  removeFavorite: (city: string) => void
+}
+
+export const useStore = create<Store>()(
+  persist(
+    (set) => ({
+      favorites: [],
+      addFavorite: (city) =>
+        set((state) => ({
+          favorites: [...new Set([...state.favorites, city])],
+        })),
+      removeFavorite: (city) =>
+        set((state) => ({
+          favorites: state.favorites.filter((c) => c !== city),
+        })),
+    }),
+    {
+      name: "weather-store",
+    }
+  )
+)
