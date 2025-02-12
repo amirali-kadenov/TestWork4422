55a4edea1e24372864a98060f08f3ec4074e5c37
Amirali <kadenov.a@mybuh.kz>
2025-02-12 18:40:12 +0500

    add: use-current-location - set city cookie, use getCitiesByCoordinates, fix isActive

--- src/features/use-current-location/index.tsx
+++ src/features/use-current-location/index.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import clsx from "clsx"
+import Cookies from "js-cookie"
 import { MapPin } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { NavLink } from "react-bootstrap"
 
+import { getCitiesByCoordinates } from "@/entities/geolocation"
 import { KEYS } from "@/shared/constants/keys"
 import { PATHS } from "@/shared/constants/paths"
 
@@ -12,40 +14,45 @@ export const UseCurrentLocation = () => {
   const router = useRouter()
   const params = useSearchParams()
 
-  const onSuccess = (position: GeolocationPosition) => {
-    const { latitude, longitude } = position.coords
-
-    const path = PATHS.FORECAST.BY_COORDINATES.get(
-      String(longitude),
-      String(latitude),
-    )
-
-    localStorage.setItem(KEYS.LAT, String(latitude))
-    localStorage.setItem(KEYS.LON, String(longitude))
-
-    router.push(path)
-  }
-
-  const onError = (error: GeolocationPositionError) => {
-    console.error("Error getting location:", error)
-  }
-
   const getLocation = () => {
     if (!navigator.geolocation) return
 
-    navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords
+
+      const lat = String(latitude)
+      const lon = String(longitude)
+
+      localStorage.setItem(KEYS.LAT, lat)
+      localStorage.setItem(KEYS.LON, lon)
+
+      const cities = await getCitiesByCoordinates(lat, lon)
+
+      const city = cities?.[0].name
+      let path = PATHS.FORECAST.BY_COORDINATES.get(lat, lon)
+
+      if (city) {
+        Cookies.set(KEYS.CITY, city, { expires: 7 })
+        path = PATHS.FORECAST.get(city)
+      }
+
+      router.push(path)
+    })
   }
 
-  const isActive =
-    params?.get(KEYS.LAT) === localStorage.getItem(KEYS.LAT) &&
-    params?.get(KEYS.LON) === localStorage.getItem(KEYS.LON)
+  const lat = params?.get(KEYS.LAT)
+  const lon = params?.get(KEYS.LON)
+  const isLatitudeMatch = lat && lat === localStorage.getItem(KEYS.LAT)
+  const isLongitudeMatch = lon && lon === localStorage.getItem(KEYS.LON)
+
+  const isActive = isLatitudeMatch && isLongitudeMatch
 
   return (
     <NavLink
       as="button"
       className={clsx(
         "d-flex align-items-center gap-2 rounded-pill",
-        isActive && "text-warning",
+        isActive && "text-success",
       )}
       onClick={getLocation}
     >
