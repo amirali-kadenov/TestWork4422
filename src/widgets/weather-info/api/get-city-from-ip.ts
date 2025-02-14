4fe15378d252afd0f5de845aa3d3689dd3ad0fd6
Amirali <kadenov.a@mybuh.kz>
2025-02-14 12:51:18 +0500

    add: get city from ipapi on client before using server

--- src/widgets/weather-info/api/get-city-from-ip.ts
+++ src/widgets/weather-info/api/get-city-from-ip.ts
@@ -4,13 +4,31 @@ type City = {
   city: string | null
 }
 
+type IpApiResponse =
+  | {
+      error: true
+      reason: string
+      message: string
+      city?: never
+    }
+  | {
+      error?: never
+      city: string
+    }
+
 export const getCityFromIp = async () => {
   try {
+    const { data } = await axios.get<IpApiResponse>("https://ipapi.co/json")
+
+    if (!data.error) {
+      return data.city
+    }
+
     const response = await axios.get<City>(`${window.location.origin}/api/city`)
 
     return response.data.city
   } catch (e) {
-    console.error(e)
+    console.warn("Could not get city from ip", e)
 
     return null
   }
